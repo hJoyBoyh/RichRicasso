5a201d56937d9e40a90abbdf84e6892afcb01bb7
Zafer A <lssjshisui@gmail.com>
2023-10-19 09:41:11 -0400

    ma connection a ma DB

--- DB.php
+++ DB.php
@@ -1,9 +1,19 @@
 <?php
-$dsn = 'mysql:host=localhost:3306;dbname=RicassoDb;charset=utf8';
+$dsn = 'mysql:host=localhost:3306;dbname=RicassoDb';
+$username = 'root';
+$password = '';
 try {
  $pdo = new PDO($dsn, $username, $password);
  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+ echo 'Connexion reussi';
 } catch (PDOException $e) {
  echo 'Échec de la connexion : ' . $e->getMessage();
 }
-?>
+/*
+$sql = "SELECT * FROM user";
+$result = $pdo->query($sql);
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+ echo $row['nom'] . ' - ' . $row['courriel'] . '<br>';
+}
+*/
